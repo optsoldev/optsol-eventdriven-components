0c088eedfef20928245f1f7960383b4feadcc74c
Carlos Machel <carlos.git@outlook.com>
2022-05-26 16:58:25 -0300

    Bugfix/correcao interface readprojection (#15)
    * Ajustes na Interface
    
    * Atualizando versao
    
    Co-authored-by: Carlos Machel <carlostiago@outlook.com.br>

--- src/Optsol.EventDriven.Components.Core.Domain/Repositories/IReadProjectionRepository.cs
+++ src/Optsol.EventDriven.Components.Core.Domain/Repositories/IReadProjectionRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Optsol.EventDriven.Components.Core.Domain.Entities;
+using Optsol.EventDriven.Components.Driven.Infra.Data.MongoDb.Repositories;
+using System.Linq.Expressions;
 
 namespace Optsol.EventDriven.Components.Core.Domain.Repositories;
 
@@ -9,4 +11,10 @@ public interface IReadProjectionRepository<T> where T : IProjection
     IEnumerable<T> GetAll();
 
     IEnumerable<T> GetAllByIds(params Guid[] ids);
+
+    IEnumerable<T> GetAll(Expression<Func<T, bool>> filterExpression);
+
+    SearchResult<T> GetAll<TSearch>(SearchRequest<TSearch> searchRequest) where TSearch : class;
+
+
 }
