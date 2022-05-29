a3e94c5960f5132a3efedeea48490258d4797e86
Carlos Machel <carlos.git@outlook.com>
2022-05-29 16:32:32 -0300

    Utilizando IFunctionContextAccessor (#18)
    Co-authored-by: Carlos Machel <carlostiago@outlook.com.br>

--- playground/EventDriven.Arch.Driving.Commands/Program.cs
+++ playground/EventDriven.Arch.Driving.Commands/Program.cs
@@ -1,18 +1,24 @@
 using EventDriven.Arch.Application;
 using EventDriven.Arch.Driven.Infra.Data.MongoDb;
+using Functions.Worker.ContextAccessor;
 using MediatR;
 using Microsoft.Extensions.Hosting;
 using Optsol.EventDriven.Components.Driven.Infra.Notification;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWorkerDefaults(worker =>
+    {
+        worker.UseFunctionContextAccessor();
+    })
     .ConfigureServices((context, services) =>
     {
         services.AddMediatR(typeof(ApplicationMediatREntryPoint).Assembly);
         services.RegisterNotification(context.Configuration);
         services.AddDataMongoModule(context.Configuration);
         services.AddApplicationModule();
+
     })
     .Build();
 
 host.Run();
+
