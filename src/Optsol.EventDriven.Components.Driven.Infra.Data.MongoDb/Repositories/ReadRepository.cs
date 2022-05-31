6dd4571f7c4382019aef17e782f8ecc6b68cbd8d
Carlos Machel <carlostiago@outlook.com.br>
2022-05-31 19:28:49 -0300

    Ajustando a ReadRepository

--- src/Optsol.EventDriven.Components.Driven.Infra.Data.MongoDb/Repositories/ReadRepository.cs
+++ src/Optsol.EventDriven.Components.Driven.Infra.Data.MongoDb/Repositories/ReadRepository.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Linq.Expressions;
 using MongoDB.Driver;
 using Optsol.EventDriven.Components.Core.Domain;
@@ -19,14 +20,8 @@ public abstract class ReadRepository<T> : IReadRepository<T> where T : IAggregat
         TransactionService = transactionService;
     }
 
-    public virtual IEnumerable<IDomainEvent> GetById(Guid id) => GetEvents(e =>
-        e.ModelId == id && e.IsStaging == false ||
-        e.TransactionId == TransactionService.GetTransactionId() && e.ModelId == id);
-
-    protected virtual IEnumerable<IDomainEvent> GetEvents(Expression<Func<PersistentEvent<IDomainEvent>, bool>> expression)
-    {
-        var sortDef = Builders<PersistentEvent<IDomainEvent>>.Sort.Descending(d => d.ModelVersion);
-
-        return Set.AsQueryable().Where(expression).OrderByDescending(s => s.ModelVersion).Select(p => p.Data).ToList();
+    public virtual IEnumerable<IDomainEvent> GetById(Guid id) {
+        return Set.AsQueryable().ToEnumerable().Where(e => e.ModelId == id && e.IsStaging == false ||
+        e.TransactionId == TransactionService.GetTransactionId() && e.ModelId == id).OrderByDescending(s => s.ModelVersion).Select(p => p.Data).ToList();
     }
 }
