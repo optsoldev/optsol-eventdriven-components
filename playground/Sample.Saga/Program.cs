b437d8a2d7536e6f78500df7a6c1feeba116d643
Carlos Machel <carlostiago@outlook.com.br>
2022-06-26 11:33:17 -0300

    Melhorias na saga

--- playground/Sample.Saga/Program.cs
+++ playground/Sample.Saga/Program.cs
@@ -21,6 +21,9 @@ var configuration = new ConfigurationBuilder()
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
+        var mongoSettings = configuration.GetSection(nameof(SagaMongoSettings)).Get<SagaMongoSettings>();
+        var rabbitMqSettings = configuration.GetSection(nameof(RabbitMqSettings)).Get<RabbitMqSettings>();
+
         services.TryAddSingleton(KebabCaseEndpointNameFormatter.Instance);
 
         services.AddHostedService<Worker>();
@@ -30,17 +33,17 @@ IHost host = Host.CreateDefaultBuilder(args)
             cfg.AddSagaStateMachine<TravelStateMachine, TravelState>()
              .MongoDbRepository(r =>
              {
-                 r.Connection = "mongodb://localhost:30001";
-                 r.DatabaseName = "saga-db";
+                 r.Connection = mongoSettings.Connection; ;
+                 r.DatabaseName = mongoSettings.DatabaseName;
                  r.CollectionName = "travel-state";
              });
 
             cfg.UsingRabbitMq((context, configurator) =>
-            {
-                configurator.Host("localhost", "/", h =>
+            {                
+                configurator.Host(rabbitMqSettings.Host, rabbitMqSettings.Vhost, h =>
                 {
-                    h.Username("guest");
-                    h.Password("guest");
+                    h.Username(rabbitMqSettings.Username);
+                    h.Password(rabbitMqSettings.Password);
                 });
                 configurator.ConfigureEndpoints(context);
             });
@@ -48,6 +51,6 @@ IHost host = Host.CreateDefaultBuilder(args)
     })
     .Build();
 
-Console.Title = "Saga Travel";
+Console.Title = configuration["Title"] ?? "Saga";
 
 await host.RunAsync();
---
+++ playground/Sample.Saga/RabbitMqSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Saga;
+
+public class RabbitMqSettings
+{
+    public string Host { get; set; }
+    public string Vhost { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
