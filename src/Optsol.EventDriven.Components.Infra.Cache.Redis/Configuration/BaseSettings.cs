d9c815814df890eb8068347d8cafd7a6ef28ffa7
Washington Borges <ton.borges@gmail.com>
2022-08-25 17:40:31 -0300

    Implementação do Redis

---
+++ src/Optsol.EventDriven.Components.Infra.Cache.Redis/Configuration/BaseSettings.cs
@@ -0,0 +1,13 @@
+﻿using Optsol.EventDriven.Components.Infra.Cache.Redis.Exceptions;
+
+namespace Optsol.EventDriven.Components.Infra.Cache.Redis.Configuration;
+
+public abstract class BaseSettings
+{
+    public abstract void Validate();
+
+    public static void ShowingException(string objectName)
+    {
+        throw new SettingsNullException(objectName);
+    }
+}
---
+++ src/Optsol.EventDriven.Components.Infra.Cache.Redis/Extensions/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+
+namespace System;
+
+public static class StringExtensions
+{
+    public static T? To<T>(this string source)
+    {
+        if (source.IsNullOrWhiteSpace())
+            return default;
+
+        return JsonSerializer.Deserialize<T>(source);
+    }
+
+    public static byte[] ToBytes(this string source)
+        => Encoding.UTF8.GetBytes(source);
+
+    public static bool IsNotNullOrWhiteSpace(this string? source)
+        => !string.IsNullOrWhiteSpace(source);
+
+    public static bool IsNullOrWhiteSpace(this string? source)
+        => string.IsNullOrWhiteSpace(source);
+}
